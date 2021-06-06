5e014c3548c3107509b6d0c4a4295596e09ff3b2
Bruno Blazeka <bruno.blazeka@gmail.com>
2021-06-06 09:25:03 +0200

    fix cors error

--- Server/handler.js
+++ Server/handler.js
@@ -15,7 +15,6 @@ const news = require('./services/news/index.js');
 const league = require('./services/league/index.js');
 const player = require('./services/player/index.js');
 const util = require('./services/util/index.js');
-let whitelist = ['http://localhost:3000', 'http://abc.com'];
 const app = express();
 
 // Construct a schema, using GraphQL schema language
@@ -52,16 +51,7 @@ var root = {
 const port = 4000;
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin 
-    if (!origin) return callback(null, true);
-    if (whitelist.indexOf(origin) === -1) {
-      var message = 'The CORS policy for this origin doesn\'t ' +
-        'allow access from the particular origin.';
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
+  credentials: true
 }));
 app.use(bodyParser.graphql());
 app.use('/graphql', graphqlHTTP({
