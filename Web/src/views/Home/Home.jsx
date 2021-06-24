3d7b6a4bbcf1c394db8c43c1cb7a131c6ca77dc5
Bruno Blazeka <bruno.blazeka@gmail.com>
2021-06-24 09:37:29 +0200

    use functions instead of classes

--- Web/src/views/Home/Home.jsx
+++ Web/src/views/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { Card, Grid, Header } from 'semantic-ui-react';
 import { IsNullOrUndefined } from '../../util/common';
 
@@ -8,53 +8,43 @@ import * as gameActions from '../../services/game';
 import * as actions from '../../services/news';
 import { GameCard, Loader, NewsFeed, NotFound, SocialFeed } from '../../components';
 
-class Home extends Component {
+export default function Home() {
 
-  componentDidMount() {
-    this.props.getGamesToday();
-    this.props.getNews('NHL');
-    this.props.getTweets('NHL');
-  }
+  const { homeNews, loadingNews, tweets, loadingTweets, games } = useSelector(state => ({
+    homeNews: state.news.news,
+    loadingNews: state.news.loadingNews,
+    tweets: state.news.tweets,
+    loadingTweets: state.news.loadingTweets,
+    games: state.game.gamesToday
+  }), shallowEqual);
 
-  render() {
-    const { homeNews, tweets, games, loadingNews, loadingTweets } = this.props;
-    if (loadingNews && loadingTweets) {
-      return (<div><Loader></Loader></div>);
-    }
-    return (
-      <div>
-        <Header as="h2">Today NHL games</Header>
-        <Card.Group>
-          {(IsNullOrUndefined(games) || games.length === 0) && <NotFound text="No games found." />}
-          {games.map((game) => {
-            return (<GameCard key={`gamecard${game.gamePk}`} game={game} />);
-          })}
-        </Card.Group>
-        <div className="news-container">
-          <Grid columns={2} stackable>
-            <Grid.Row>
-              <Grid.Column><NewsFeed news={homeNews}></NewsFeed></Grid.Column>
-              <Grid.Column><SocialFeed tweets={tweets}></SocialFeed></Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      </div>);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(gameActions.getGamesToday());
+    dispatch(actions.getNews('NHL'));
+    dispatch(actions.getTweets('NHL'));
+  }, [dispatch]);
+
+  if (loadingNews && loadingTweets) {
+    return (<div><Loader></Loader></div>);
   }
+  return (
+    <div>
+      <Header as="h2">Today NHL games</Header>
+      <Card.Group>
+        {(IsNullOrUndefined(games) || games.length === 0) && <NotFound text="No games found." />}
+        {games.map((game) => {
+          return (<GameCard key={`gamecard${game.gamePk}`} game={game} />);
+        })}
+      </Card.Group>
+      <div className="news-container">
+        <Grid columns={2} stackable>
+          <Grid.Row>
+            <Grid.Column><NewsFeed news={homeNews}></NewsFeed></Grid.Column>
+            <Grid.Column><SocialFeed tweets={tweets}></SocialFeed></Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    </div>);
 }
-
-const mapStateToProps = state => ({
-  homeNews: state.news.news,
-  loadingNews: state.news.loadingNews,
-  teams: state.team.teams,
-  tweets: state.news.tweets,
-  loadingTweets: state.news.loadingTweets,
-  games: state.game.gamesToday
-});
-
-const mapDispatchToProps = dispatch => ({
-  getNews: (query) => dispatch(actions.getNews(query)),
-  getTweets: (query) => dispatch(actions.getTweets(query)),
-  getGamesToday: () => dispatch(gameActions.getGamesToday())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- Web/src/views/Standings/Standings.jsx
+++ Web/src/views/Standings/Standings.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Grid, Header, Label, Segment, Table } from 'semantic-ui-react';
 
@@ -10,94 +10,86 @@ import { Loader, Map } from '../../components';
 import routes from '../../routes';
 import { getLogo } from '../../util/assets';
 
-class Standings extends Component {
+export default function Standings() {
 
-  componentDidMount() {
-    this.props.getStandings();
-    this.props.getTeamLocations();
+  const { standings, locations } = useSelector(state => ({
+    standings: state.league.standings,
+    locations: state.team.locations,
+  }), shallowEqual);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.getStandings());
+    dispatch(teamActions.getTeamLocations());
+  }, [dispatch]);
+
+  if (!standings) {
+    return (<Loader text="Loading standings..."></Loader>);
   }
-
-  render() {
-    const { locations, standings } = this.props;
-    if (!standings) {
-      return (<Loader text="Loading standings..."></Loader>);
-    }
-    return (
-      <div>
-        <Grid columns={2} stackable>
-          {standings && standings.map((entry) => {
-            return (
-              <Grid.Column className="standings" key={entry.division.id}>
-                <Header as='h3'>{entry.division.name}</Header>
-                <Table celled>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell>Rank</Table.HeaderCell>
-                      <Table.HeaderCell>Logo</Table.HeaderCell>
-                      <Table.HeaderCell>Team</Table.HeaderCell>
-                      <Table.HeaderCell>GP</Table.HeaderCell>
-                      <Table.HeaderCell>W</Table.HeaderCell>
-                      <Table.HeaderCell>L</Table.HeaderCell>
-                      <Table.HeaderCell>OT</Table.HeaderCell>
-                      <Table.HeaderCell>GS</Table.HeaderCell>
-                      <Table.HeaderCell>GA</Table.HeaderCell>
-                      <Table.HeaderCell>PTS</Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {entry.teamRecords.map((record) => {
-                      return (
-                        <Table.Row key={`row${record.team.id}`}>
-                          <Table.Cell className="bold">{record.divisionRank}</Table.Cell>
-                          <Table.Cell>
-                            <Link to={`${routes.teams}/${record.team.id}`}><img className="logo" src={getLogo(record.team.id)} alt={`img${record.team.Id}`}></img></Link>
-                          </Table.Cell>
-                          <Table.Cell className="standings-team-name">
-                            <Link to={`${routes.teams}/${record.team.id}`}>{record.team.name}</Link>
-                          </Table.Cell>
-                          <Table.Cell>{record.gamesPlayed}</Table.Cell>
-                          <Table.Cell>{record.leagueRecord.wins}</Table.Cell>
-                          <Table.Cell>{record.leagueRecord.losses}</Table.Cell>
-                          <Table.Cell>{record.leagueRecord.ot}</Table.Cell>
-                          <Table.Cell>{record.goalsScored}</Table.Cell>
-                          <Table.Cell>{record.goalsAgainst}</Table.Cell>
-                          <Table.Cell>{record.points}</Table.Cell>
-                        </Table.Row>
-                      );
-                    })}
-                  </Table.Body>
-                </Table></Grid.Column>);
-          })}
-        </Grid>
-        <Segment>
-          {locations &&
-            <Grid columns={2} stackable>
-              <Grid.Row>
-                <Grid.Column>
-                  <Map className="standingsMapControl" center={{ center: [-97.131087, 42.509726] }} points={locations.teamLocations} zoom={2.5} />
-                </Grid.Column>
-                <Grid.Column>
-                  {locations.seasonDescription}
-                  {locations.divisions.map((marker, index) => {
-                    return (<div key={marker.key + index}><Label color={marker.value}>{marker.key}</Label></div>);
+  return (
+    <div>
+      <Grid columns={2} stackable>
+        {standings && standings.map((entry) => {
+          return (
+            <Grid.Column className="standings" key={entry.division.id}>
+              <Header as='h3'>{entry.division.name}</Header>
+              <Table celled>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Rank</Table.HeaderCell>
+                    <Table.HeaderCell>Logo</Table.HeaderCell>
+                    <Table.HeaderCell>Team</Table.HeaderCell>
+                    <Table.HeaderCell>GP</Table.HeaderCell>
+                    <Table.HeaderCell>W</Table.HeaderCell>
+                    <Table.HeaderCell>L</Table.HeaderCell>
+                    <Table.HeaderCell>OT</Table.HeaderCell>
+                    <Table.HeaderCell>GS</Table.HeaderCell>
+                    <Table.HeaderCell>GA</Table.HeaderCell>
+                    <Table.HeaderCell>PTS</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {entry.teamRecords.map((record) => {
+                    return (
+                      <Table.Row key={`row${record.team.id}`}>
+                        <Table.Cell className="bigger-text bold">{record.divisionRank}</Table.Cell>
+                        <Table.Cell>
+                          <Link to={`${routes.teams}/${record.team.id}`}><img className="logo" src={getLogo(record.team.id)} alt={`img${record.team.Id}`}></img></Link>
+                        </Table.Cell>
+                        <Table.Cell className="standings-team-name">
+                          <Link to={`${routes.teams}/${record.team.id}`}>{record.team.name}</Link>
+                        </Table.Cell>
+                        <Table.Cell>{record.gamesPlayed}</Table.Cell>
+                        <Table.Cell>{record.leagueRecord.wins}</Table.Cell>
+                        <Table.Cell>{record.leagueRecord.losses}</Table.Cell>
+                        <Table.Cell>{record.leagueRecord.ot}</Table.Cell>
+                        <Table.Cell>{record.goalsScored}</Table.Cell>
+                        <Table.Cell>{record.goalsAgainst}</Table.Cell>
+                        <Table.Cell className="bold">{record.points}</Table.Cell>
+                      </Table.Row>
+                    );
                   })}
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>}
-        </Segment>
-      </div>
-    );
-  }
+                </Table.Body>
+              </Table></Grid.Column>);
+        })}
+      </Grid>
+      <Segment>
+        {locations &&
+          <Grid columns={2} stackable>
+            <Grid.Row>
+              <Grid.Column>
+                <Map className="standingsMapControl" center={{ center: [-97.131087, 42.509726] }} points={locations.teamLocations} zoom={2.5} />
+              </Grid.Column>
+              <Grid.Column>
+                {locations.seasonDescription}
+                {locations.divisions.map((marker, index) => {
+                  return (<div key={marker.key + index}><Label color={marker.value}>{marker.key}</Label></div>);
+                })}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>}
+      </Segment>
+    </div>
+  );
 }
-
-const mapStateToProps = state => ({
-  standings: state.league.standings,
-  locations: state.team.locations,
-});
-
-const mapDispatchToProps = dispatch => ({
-  getStandings: () => dispatch(actions.getStandings()),
-  getTeamLocations: () => dispatch(teamActions.getTeamLocations()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Standings);
