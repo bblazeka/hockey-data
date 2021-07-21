980a66946afa16e19a979dc9e28f06b4a48f680a
Bruno Blazeka <bruno.blazeka@gmail.com>
2021-07-21 12:26:24 +0200

    testing game card

--- Web/src/components/GameCard/GameCard.test.js
+++ Web/src/components/GameCard/GameCard.test.js
@@ -6,6 +6,20 @@ import GameCard from './GameCard';
 
 describe('GameCard component', () => {
   let container = null;
+  let baseGame = {
+    teams: {
+      home: {
+        team: { id: 1, name: 'Test 1' },
+        shotsOnGoal: 1,
+        goals: 1
+      },
+      away: {
+        team: { id: 2, name: 'Test 2' },
+        shotsOnGoal: 2,
+        goals: 2
+      }
+    }
+  };
   beforeEach(() => {
     // setup a DOM element as a render target
     container = document.createElement('div');
@@ -26,5 +40,56 @@ describe('GameCard component', () => {
     });
 
     expect(container.textContent).toBe('Loading...');
+  });
+
+  it('renders ongoing game', () => {
+    var currentTime = '10:00';
+    var currentPeriod = '3rd';
+
+    var game = Object.assign(baseGame, {
+      ongoingGame: true,
+      finished: false,
+      currentPeriodOrdinal: currentPeriod,
+      currentPeriodTimeRemaining: currentTime
+    });
+
+    act(() => {
+      render(<GameCard game={game} />, container);
+    });
+
+    expect(container.textContent).toContain(currentTime);
+    expect(container.textContent).toContain(currentPeriod);
+  });
+
+  it('renders planned game', () => {
+    const gameTime = '10:00';
+    var game = Object.assign(baseGame, {
+      ongoingGame: false,
+      finished: false,
+      gameTime
+    });
+    
+    act(() => {
+      render(<GameCard game={game} />, container);
+    });
+
+    expect(container.textContent).toContain(gameTime);
+  });
+
+  it('renders finished game', () => {
+    const finalLabel = 'Final';
+
+    var game = Object.assign(baseGame, {
+      ongoingGame: true,
+      currentPeriodOrdinal: '3rd',
+      currentPeriodTimeRemaining: finalLabel,
+      finished: true
+    });
+    
+    act(() => {
+      render(<GameCard game={game} />, container);
+    });
+
+    expect(container.textContent).toContain(finalLabel);
   });
 });
