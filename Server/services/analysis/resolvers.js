ee74f941dbcfbc509f34cb6ad9088ed9c5b7882b
Bruno Blazeka <bruno.blazeka@gmail.com>
2021-10-06 20:26:55 +0200

    bugfix

--- Server/services/analysis/resolvers.js
+++ Server/services/analysis/resolvers.js
@@ -20,11 +20,9 @@ async function getAnalysis() {
         stats: p.stats,
       });
     });
-    team.skaterStats =
-      team.rosterStats?.filter((s) => "shifts" in s.stats) ?? [];
+    team.skaterStats = team.rosterStats?.filter((s) => s.stats["shifts"]) ?? [];
     team.skaterStats.sort((p1, p2) => p2.stats.points - p1.stats.points);
-    team.goalieStats =
-      team.rosterStats?.filter((s) => "saves" in s.stats) ?? [];
+    team.goalieStats = team.rosterStats?.filter((s) => s.stats["saves"]) ?? [];
     team.goalieStats.sort((p1, p2) => p2.stats.wins - p1.stats.wins);
     Object.assign(team, {
       rankingsGraph: [
