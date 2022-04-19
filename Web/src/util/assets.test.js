18da5ce61c89f3dded716cba0f8fd6e49ed31dac
Bruno Blazeka <bruno.blazeka@gmail.com>
2022-04-19 19:06:13 +0200

    fix tests

--- Web/src/util/assets.test.js
+++ Web/src/util/assets.test.js
@@ -6,9 +6,9 @@ describe('Assets methods', () => {
     mockGetLogo(1);
     mockGetLogo(10);
     mockGetLogo(55);
-    mockGetLogo(56);
+    mockGetLogo(58);
 
     const result = mockGetLogo.mock.results.map(r => r.value);
-    expect(result).toEqual(['/assets/logos/njd.webp', '/assets/logos/tor.webp', '/assets/logos/sea.webp', null]);
+    expect(result).toEqual(['/assets/logos/njd.webp', '/assets/logos/tor.webp', '/assets/logos/sea.webp', "/logo512.png"]);
   });
 });
