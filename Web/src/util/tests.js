dbf4e667a64a0e8bd70702a93593707a362a4662
Bruno Blazeka <bruno.blazeka@gmail.com>
2022-06-12 08:51:02 +0200

    use sagas in tests

--- Web/src/util/tests.js
+++ Web/src/util/tests.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from 'store';
+import createSagaMiddleware from "redux-saga";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+import saga from "reducers/sagas";
+import rootReducer from "reducers/rootReducer";
 
 export const renderWithState = (
   ui,
   { ...renderOptions } = {}
 ) => {
-  const store = configureStore();
+  const sagaMiddleware = createSagaMiddleware();
+  const middleWares = [sagaMiddleware, thunk];
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleWares)));
+  sagaMiddleware.run(saga);
   const Wrapper = ({ children }) => (
     <Provider store={store}>
       <Router>{children}
